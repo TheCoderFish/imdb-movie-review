cde7d16125a5ce98722ee79509edb52aca0214e7
Nurali Khoja <nuralikhoja@gmail.com>
2020-01-21 16:47:21 +0530

    initial setup with routing and module generation

---
+++ src/app/movies/movie-review/movie-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-review',
+  templateUrl: './movie-review.component.html',
+  styleUrls: ['./movie-review.component.scss']
+})
+export class MovieReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/movies/movies.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MoviesRoutingModule } from './movies-routing.module';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieReviewComponent } from './movie-review/movie-review.component';
+
+
+@NgModule({
+  declarations: [MoviesListComponent, MovieDetailComponent, MovieReviewComponent],
+  imports: [
+    CommonModule,
+    MoviesRoutingModule
+  ],
+  entryComponents:[
+    MovieReviewComponent
+  ]
+})
+export class MoviesModule { }
